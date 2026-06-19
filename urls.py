174c30a56bbbbb14b5f358330a2ae70c8b0bf866
saurabhdhage34 <saurabhdhage20114@gmail.com>
2026-06-19 12:34:19 +0530

    Clean up and organize URL patterns in urls.py

--- urls.py
+++ urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for student_management_system project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from student_management_app import views
@@ -27,7 +12,7 @@ urlpatterns = [
 
      
 
-    # path('student_home_1/', views.student_home_1, name='student_home_1'),
+   
     path('student_home_1/<int:id>/',views.student_home_1, name='student_home_1'),
 
     path('home/', views.Homepage, name='home'),
@@ -85,7 +70,7 @@ urlpatterns = [
 
 
 
-    # ================= DEFAULT PAGE =================
+  
 
 
     # ================= STUDENT LOGIN =================
